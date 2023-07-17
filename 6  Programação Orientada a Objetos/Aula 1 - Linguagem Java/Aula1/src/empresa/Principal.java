2a2889a99ff25979c883b9ee5a42ef52c6173bc0
Wesley Alexandre <wesley.lopes14@hotmail.com>
2023-07-17 14:41:17 -0300

    Visão geral sobre o Java

---
+++ 6  Programação Orientada a Objetos/Aula 1 - Linguagem Java/Aula1/src/empresa/Principal.java
@@ -0,0 +1,12 @@
+package empresa;
+
+public class Principal {
+// Array
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Bye World");
+
+	}
+
+}
