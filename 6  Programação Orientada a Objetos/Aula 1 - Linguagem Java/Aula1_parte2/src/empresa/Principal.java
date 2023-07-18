d983c3d16e3f6d3b48844ba428c15f0cd1a41857
Wesley Alexandre <wesley.lopes14@hotmail.com>
2023-07-18 19:50:30 -0300

    Finalizando Aula 1

---
+++ 6  Programação Orientada a Objetos/Aula 1 - Linguagem Java/Aula1_parte2/src/empresa/Principal.java
@@ -0,0 +1,22 @@
+package empresa;
+
+public class Principal {
+// Array
+	public static void main(String[] args) {
+		
+		int idade = 10;
+		
+		idade = idade + 2;
+		double peso = 80.5;
+		
+		System.out.println("Idade: " + idade);
+		System.out.printf("Peso: %.2f\n", peso);
+		System.out.printf("Idade: %d\n", idade);
+		
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Bye World");
+
+	}
+
+}
