ff8cdc267d1e8b85a10855bc3a56716561824a47
Wesley Alexandre <wesley.lopes14@hotmail.com>
2023-07-21 05:08:25 -0300

    Interação entre objetos

---
+++ 6  Programação Orientada a Objetos/Aula 2 - Classes e Objetos/3 padrões e modificador static/src/aula_prática1/Principal.java
@@ -0,0 +1,11 @@
+
+ package aula_prática1;
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Carro c = new Carro();
+		System.out.println(Carro.milhasParaMetros(10));
+	}
+}
